792df336df4d66c123c307ac5a0eddf54863dc2c
DannyBoyNYC <daniel.deverell@gmail.com>
2019-04-10 14:25:25 -0400

    done

--- pages/home.md
+++ pages/home.md
@@ -1,8 +1,6 @@
 ---
-layout: layouts/layout.html
+layout: layouts/home.html
 pageTitle: Home
-tags:
-  - nav
 navTitle: Home
 date: 2010-01-01
 permalink: /
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,46 +1,68 @@
+const carouselLinks = document.querySelectorAll('.image-tn a');
+const carouselLinksArray = [...carouselLinks];
+const carousel = document.querySelector('figure img');
+const carouselPara = document.querySelector('figcaption');
 
+carouselLinksArray.forEach(carouselLink =>
+  carouselLink.addEventListener('click', runCarousel),
+);
 
+function runCarousel() {
+  const imageHref = event.target.parentNode.getAttribute('href');
+  console.log(imageHref);
+  const titleText = event.target.title;
+  console.log(titleText);
+  carousel.setAttribute('src', imageHref);
+  carouselPara.innerHTML = titleText;
+  event.preventDefault();
+}
 //////
 
-document.addEventListener('click', clickHandlers)
+document.addEventListener('click', clickHandlers);
 
-var nyt = 'https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=OuQiMDj0xtgzO80mtbAa4phGCAJW7GKa'
+var nyt =
+  'https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=OuQiMDj0xtgzO80mtbAa4phGCAJW7GKa';
 
-function clickHandlers(){
-  if (event.target.matches('#pull')){
+function clickHandlers() {
+  if (event.target.matches('#pull')) {
     document.querySelector('body').classList.toggle('show-nav');
     event.preventDefault();
   }
-  if (event.target.matches('.content-video a')){
-    const iFrame = document.querySelector('iframe');
-    const videoLinks = document.querySelectorAll('.content-video a');
-    videoLinks.forEach(videoLink => videoLink.classList.remove('active'));
-    event.target.classList.add('active');
-    const videoToPlay = event.target.getAttribute('href');
-    iFrame.setAttribute('src', videoToPlay);
+  if (event.target.matches('.content-video a')) {
+    videoSwitch();
     event.preventDefault();
   }
 }
 
-var addContent = function(data){
+var videoSwitch = function() {
+  const iFrame = document.querySelector('iframe');
+  const videoLinks = document.querySelectorAll('.content-video a');
+  videoLinks.forEach(videoLink => videoLink.classList.remove('active'));
+  event.target.classList.add('active');
+  const videoToPlay = event.target.getAttribute('href');
+  iFrame.setAttribute('src', videoToPlay);
+};
 
-  var looped = ''
+var addContent = function(data) {
+  var looped = '';
 
-  for(i=0; i<data.results.length; i++){
+  for (i = 0; i < data.results.length; i++) {
     looped += `
       <div class="item">
         <h3>${data.results[i].title}</h3>
         <p>${data.results[i].abstract}</p>
       </div>
-      `
+      `;
   }
-  document.querySelector('.content div').innerHTML = looped
-}
+  if (document.querySelector('.content .blog')) {
+    document.querySelector('.content .blog').innerHTML = looped;
+  }
+};
 
-var getData = function () {
-	fetch(nyt)
-  .then(response => response.json())
-  .then(json => addContent(json))
-}
+var getData = function() {
+  fetch(nyt)
+    .then(response => response.json())
+    .then(json => addContent(json));
+};
 
 getData();
